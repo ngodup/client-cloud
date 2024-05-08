9b183bb5108811dd0a536608c6203c5b7af2219d
ngodup <ngodupbz@gmail.com>
2024-05-08 12:07:54 +0200

    Improve the product code

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,4 +1,10 @@
-import React, { useContext, useMemo, useState, useEffect } from "react";
+import React, {
+  useContext,
+  useMemo,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
 import axios from "axios";
 import { IoMdCloseCircle } from "react-icons/io";
 import CustomModal from "../shared/CustomModal/CustomModal";
@@ -31,7 +37,13 @@ const Products: React.FC<ProductsProps> = ({ filteredProducts }) => {
   const [selectedCommentToEdit, setSelectedCommentToEdit] =
     useState<Comment | null>(null);
 
-  const { userResponse, isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, userResponse } = useContext(AuthContext);
+  const user = userResponse?.user;
+
+  const memoizedProductComments = useMemo(
+    () => productComments,
+    [productComments]
+  );
 
   useEffect(() => {
     let isMounted = true; // to prevent setting state of an unmounted component
@@ -65,95 +77,120 @@ const Products: React.FC<ProductsProps> = ({ filteredProducts }) => {
 
   const productQuantity = useMemo(() => {
     if (selectedProduct) {
-      const item = cartItems.find(
+      const itemIndex = cartItems.findIndex(
         (cartItem) => cartItem.product.id === selectedProduct.id
       );
-      return item ? item.quantity : 0;
+      return itemIndex !== -1 ? cartItems[itemIndex].quantity : 0;
     }
     return 0;
   }, [cartItems, selectedProduct]);
 
-  if (filteredProducts?.length === 0) {
-    return null;
-  }
+  const handleAddToCart = useCallback(
+    (product: Product) => {
+      const productExists = cartItems.some(
+        (item) => item.product.id === product.id
+      );
+      if (!productExists) {
+        dispatch(addToCart(product));
+      }
+    },
+    [cartItems, dispatch]
+  );
 
-  const handleAddToCart = (product: Product) => {
-    dispatch(addToCart(product));
-  };
-
-  const handleRemoveFromCart = (product: Product) => {
-    dispatch(removeFromCart(product));
-  };
+  const handleRemoveFromCart = useCallback(
+    (product: Product) => {
+      dispatch(removeFromCart(product));
+    },
+    [dispatch]
+  );
   // End of the code for Modal cart increment and Decrment Quantity
 
   //CODE TO FETCH comment on the product.
-  const handleAddComment = async (content: string, productId: number) => {
-    // const userId = userResponse?.user.id;
-    const token = localStorage.getItem("token"); // Or however you're storing the token
-    if (!token) {
-      return;
-    }
-    try {
-      const response = await addComment(productId, content, token);
-      alert("Add comment on a product successful");
-      if (response) {
-        setProductComments([response, ...productComments]);
+  const handleAddComment = useCallback(
+    async (content: string, productId: number) => {
+      const token = localStorage.getItem("token"); // Or however you're storing the token
+      if (!token) {
+        return;
       }
-    } catch (error) {
-      alert("Error on adding comment on a product successful " + error);
-    }
-  };
+      try {
+        const response = await addComment(productId, content, token);
+        alert("Add comment on a product successful");
+        if (response) {
+          setProductComments([response, ...memoizedProductComments]);
+        }
+      } catch (error) {
+        alert("Error on adding comment on a product successful " + error);
+      }
+    },
+    [memoizedProductComments]
+  );
 
   // CODE TO EDIT THE COMMENT ON A PRODUCT
-  const editComment = (comment: Comment) => {
+  const editComment = useCallback((comment: Comment) => {
     setSelectedCommentToEdit(comment);
-  };
+  }, []);
 
-  const onUpdatedComment = async (content: string, commentId: number) => {
-    //Call to api
-    const token = localStorage.getItem("token"); // Or however you're storing the token
-    if (!token) {
-      return;
-    }
-    try {
-      await updateComment(commentId, content, token);
-      setProductComments(
-        productComments.map((comment) =>
-          comment.id === commentId ? { ...comment, content } : comment
-        )
-      );
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error("Error editing comment:", error.message);
-      } else {
-        console.error("Unexpected error:", error);
+  const onUpdatedComment = useCallback(
+    async (content: string, commentId: number) => {
+      //Call to api
+      const token = localStorage.getItem("token"); // Or however you're storing the token
+      if (!token) {
+        return;
       }
-    }
-  };
+      try {
+        await updateComment(commentId, content, token);
+        const commentIndex = memoizedProductComments.findIndex(
+          (comment) => comment.id === commentId
+        );
+        if (commentIndex !== -1) {
+          const updatedComments = [...memoizedProductComments];
+          updatedComments[commentIndex] = {
+            ...updatedComments[commentIndex],
+            content,
+          };
+          setProductComments(updatedComments);
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error("Error editing comment:", error.message);
+        } else {
+          console.error("Unexpected error:", error);
+        }
+      }
+    },
+    [memoizedProductComments]
+  );
   // CODE TO DELETE THE COMMENT ON A PRODUCT
-  const deleteComment = async (commentId: number) => {
-    const token = localStorage.getItem("token"); // Or however you're storing the token
-    if (!token) {
-      return;
-    }
-    try {
-      await deleteAComment(commentId, token);
-      setProductComments(
-        productComments.filter((comment) => comment.id !== commentId)
-      );
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error("Error deleting comment:", error.message);
-      } else {
-        console.error("Unexpected error:", error);
+  const deleteComment = useCallback(
+    async (commentId: number) => {
+      const token = localStorage.getItem("token"); // Or however you're storing the token
+      if (!token) {
+        return;
       }
-    }
-  };
+      try {
+        await deleteAComment(commentId, token);
+        setProductComments(
+          memoizedProductComments.filter((comment) => comment.id !== commentId)
+        );
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error("Error deleting comment:", error.message);
+        } else {
+          console.error("Unexpected error:", error);
+        }
+      }
+    },
+    [memoizedProductComments]
+  );
+
+  if (!filteredProducts || filteredProducts.length === 0) {
+    return null;
+  }
 
   return (
     <>
       <section className="card-container">
-        {filteredProducts?.map((product: Product, index: number) => (
+        {filteredProducts.map((product: Product, index: number) => (
           <ProductCard
             key={index}
             product={product}
@@ -223,15 +260,15 @@ const Products: React.FC<ProductsProps> = ({ filteredProducts }) => {
           </div>
 
           <div>
-            {productComments &&
-              productComments.length > 0 &&
-              productComments.map((comment: Comment) => (
+            {memoizedProductComments &&
+              memoizedProductComments.length > 0 &&
+              memoizedProductComments.map((comment: Comment) => (
                 <div key={comment.id}>
                   <CommentCard
                     comment={comment}
                     onEdit={editComment}
                     onDelete={deleteComment}
-                    user={userResponse?.user}
+                    user={user}
                   />
                 </div>
               ))}
